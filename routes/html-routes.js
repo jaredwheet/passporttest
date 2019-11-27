f13f045ff793c4dd13b08b552dea8b9b0607ae3d
Jared Wheet <jaredwheet@gmail.com>
2019-11-27 15:41:15 -0700

    working on the html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,16 +7,12 @@ module.exports = function(app) {
 //
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
+
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 //
   app.get("/login", function(req, res) {
-    if (req.user) {
-      res.redirect("/members");
-    }
+
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 //
